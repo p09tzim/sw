434638f4c41758e26200834fe853a2c9661333fd
p13kara33 <p13kara3@ionio.gr>
2016-03-19 20:27:54 +0200

    2ο Παραδοτεο
    2ο Παραδοτεο

---
+++ projects_2016/Π2013098-Π2013090-Π2013017/New Unity Project/Assets/Camera_controller.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera_controller : MonoBehaviour {
+
+    public GameObject player;
+
+    private Vector3 offset;
+
+	void Start ()
+    {
+        offset = transform.position - player.transform.position;
+	
+	}
+	
+	// Update is called once per frame
+	void LateUpdate ()
+    {
+        transform.position = player.transform.position + offset;
+
+
+        
+	
+	}
+}
